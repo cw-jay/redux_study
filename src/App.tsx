4fdaf9981165a61ca221632ffaf2b454b05d111d
cw-jay <cw-jay@coway.com>
2022-01-18 18:49:54 +0900

    feat(#1): redux toy project 를 위한 환경 세팅
    - redux 및 redux tool kit 디펜던시 추가
    - react 및 관련 package 추가

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+        <Counter />
+  );
+}
+
+export default App;
